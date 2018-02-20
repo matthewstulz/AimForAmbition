dca7e3145a2d133bc15505a56ed021484462ad0c
matthewstulz <stulzm2@nku.edu>
2018-02-20 10:56:03 -0500

    Flushed out about activity

--- app/src/main/java/com/github/stulzm2/aimforambition/AboutActivity.kt
+++ app/src/main/java/com/github/stulzm2/aimforambition/AboutActivity.kt
@@ -1,8 +1,11 @@
 package com.github.stulzm2.aimforambition
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
@@ -11,10 +14,30 @@ class AboutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_about)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         initViews()
+        initOperations()
     }
 
     private fun initViews() {
         supportActionBar?.title = "About"
+    }
+
+    private fun initOperations() {
+        button_url_github.setOnClickListener({
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_github)))
+            startActivity(intent)
+        })
+
+        button_url_canva.setOnClickListener({
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_logo)))
+            startActivity(intent)
+        })
+
+        button_email.setOnClickListener({
+            val intent = Intent(Intent.ACTION_VIEW)
+            val data = Uri.parse("mailto:mstulz2@gmail.com?subject=&body=")
+            intent.data = data
+            startActivity(intent)
+        })
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
